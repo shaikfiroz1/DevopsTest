8b6ffe59036086bab7a722cd952909fc2541a440
firozdravid1@gmail.com <firozdravid1@gmail.com>
2025-10-08 21:32:19 +0530

    test

---
+++ new.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send('Parimalla enduku parimalla intha deggara ayyav asala mental dana ');
+});
+
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
